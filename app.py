92de1b9f622f6a6b53e40667d3e02ca11bc5d4d6
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2022-08-03 00:19:15 +0800

    Add selection sort route

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ def index():
 @app.route("/bubble_sort")
 def bubble_render():
     return render_template("core/sort/bubble_sort.html")
+
+@app.route("/selection_sort")
+def selection_render():
+    return render_template("core/sort/selection_sort.html")
+    
