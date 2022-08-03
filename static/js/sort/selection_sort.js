3579656f1df79e9e232e8fddebfdf811d48dc12c
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2022-08-04 01:40:17 +0800

    Update selection sort

--- static/js/sort/selection_sort.js
+++ static/js/sort/selection_sort.js
@@ -3,6 +3,7 @@ import { BlockGraphFactory } from "../lib/graph.js";
 import { disableControl, enableControl } from "../lib/nodectrl.js";
 
 
+var BIAS = 5
 // Namespace URI for createElementNS
 var svgns = "http://www.w3.org/2000/svg";
 
@@ -20,10 +21,9 @@ function drawNewGraph(size) {
     buildGraph.draw();
 }
 
-drawNewGraph(5)
+drawNewGraph(30)
 // Trigger new graph drawing button
 let submitSize = document.getElementById("submitSize");
-console.log(submitSize);
 submitSize.addEventListener("click", function() {
     new_graph.clearGraph();
     drawNewGraph(document.getElementById("inputSize").value);
@@ -37,7 +37,7 @@ submitSize.addEventListener("click", function() {
  */
 
 // Find the minimum value
-function findMin(array, start, end, delay) {
+async function findMin(array, start, end, delay) {
     let min = array[start];
     min.style = "fill: #81C784";
     // Remove the color in the previous block
@@ -48,25 +48,30 @@ function findMin(array, start, end, delay) {
     
     for (let i = start+1; i <= end; i++)
     {
-        setTimeout(() => {
-            // console.log("Inner " + i.toString() + ": " + ((i-start)*delay).toString());
-            // Track the running block
-            array[i-1].removeAttribute("style");
-            array[i].style = "fill: #FFEE58";
-            // Ensure the min block still keeps its color
-            min.style = "fill: #81C784";
-            if (parseInt(array[i].id) < parseInt(min.id))
-            {
-                min.removeAttribute("style");
-                min = array[i];
-                min.style = "fill: #81C784";               
-            }
-            // Swap the minimum value with the first unsorted point
-            if (i === end)
-            {
-                swap(array[start], min);  
-            }
-        }, (i-start) * delay);
+        // console.log("Inner " + i.toString() + ": " + ((i-start)*delay).toString());
+        await new Promise((resolve) => 
+            setTimeout(() => {
+                resolve();
+                // console.log("pending" + i +"    "+Date.now())
+            }, delay)
+        );
+        // Track the running block
+        array[i-1].removeAttribute("style");
+        array[i].style = "fill: #FFEE58";
+        // Ensure the min block still keeps its color
+        min.style = "fill: #81C784";
+        if (parseInt(array[i].id) < parseInt(min.id))
+        {
+            min.removeAttribute("style");
+            min = array[i];
+            min.style = "fill: #81C784";               
+        }
+        // Swap the minimum value with the first unsorted point
+        if (i === end)
+        {
+            array[end].removeAttribute("style");
+            swap(array[start], min);
+        }
     }
 }
 
@@ -80,15 +85,13 @@ function selectionSort(arr, delay) {
     let l = arr.length;
     for (let n = 0; n < l; n++)
     {
+        // console.log("n: "+n);
+        // Remove the track color in the last box
+        // arr[l-1].removeAttribute("style");
         setTimeout(() => {
-            // console.log("Outer " + n.toString() + ": " + ((getSteps(n, l-1)+n) * delay).toString());
-            // Remove the track color in the last box whenever 
-            arr[l-1].removeAttribute("style");
+            // console.log("Outer " + n.toString() + ": " + ((getSteps(n, l-1)+n) * delay).toString()+"    "+Date.now());
             // Find the minimum value in a range
-            setTimeout(() => {
-                findMin(arr, n, l-1, delay);
-            }, delay);
-            
+            findMin(arr, n, l-1, delay);
             if (n === l - 1)
             {
                 // Remove the minimum value block's color when it reaches the last block
@@ -96,9 +99,9 @@ function selectionSort(arr, delay) {
                 setTimeout(() => {
                     enableControl();
                     arr[l-1].removeAttribute("style");
-                }, 2*delay);
+                }, delay);
             }
-        }, ((getSteps(n, l-1)+n) * delay)+((n+1)*delay));
+        }, getSteps(n, l)*(delay+BIAS))
     }
 }
 
