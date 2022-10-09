5473f06c3cf97031d824407e50552a04f70e2be9
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2022-10-09 23:06:41 +0800

    Update merge sort

--- static/js/sort/merge_sort.js
+++ static/js/sort/merge_sort.js
@@ -1,4 +1,4 @@
-import { swap, getDelay } from "../lib/cust_func_lib.js";
+import { getDelay } from "../lib/cust_func_lib.js";
 import { BlockGraphFactory } from "../lib/graph.js";
 import { enableControl, disableControl } from "../lib/nodectrl.js";
 
@@ -10,6 +10,9 @@ const canvas = document.getElementById("graph");
 const svgWidth = parseInt(canvas.clientWidth);
 const svgHigh = parseInt(canvas.clientHeight) * 0.85;
 
+/* Declare delay for animation */
+var DELAY;
+
 /* Build graph */
 let new_graph;
 function drawNewGraph(size) {
@@ -20,7 +23,7 @@ function drawNewGraph(size) {
 }
 
 // Initial drawing
-drawNewGraph(10)
+drawNewGraph(50)
 
 /* Trigger new graph drawing button */
 const submitSize = document.getElementById("submitSize");
@@ -32,20 +35,98 @@ submitSize.addEventListener("click", function () {
 /* Implement Merge Sort */
 /**
  * For visualization
- *  -   
+ *  -   Mid block: #72A1EF
+ *  -   Start block: #81C784
+ *  -   End block: #FFEE58
  */
-function mergeSort(delay, arr) {
+async function mergeSort(arr, start, end) {
+    if (start < end)
+    {
+        await new Promise((resolve) =>
+            setTimeout(() => {
+                resolve();
+            }, DELAY)
+        );
+        arr[start].style = "fill: #81C784; ";
+        arr[end].style = "fill: #FFEE58; ";
+        // Init start, end, mid var
+        let mid = Math.floor((start + end) / 2);
+        arr[mid].style = "fill: #72A1EF; ";
+        // Perform merge sort in 2 halves
+        await mergeSort(arr, start, mid);
+        await mergeSort(arr, mid + 1, end)
+        // Call merge 
+        await merge(arr, start, mid, end)
+        await new Promise((resolve) =>
+            setTimeout(() => {
+                resolve();
+            }, DELAY)
+        );
+        arr[start].removeAttribute("style");
+        arr[mid].removeAttribute("style");
+        arr[end].removeAttribute("style");
+    }
+    if (start === 0 && end === arr.length - 1)
+    {
+        enableControl();
+    }
+}
 
+async function merge(arr, left, mid, right) {
+    // Init a copy of arr: only object {height, id}
+    let arrCopy = new Array(arr.length);
+    let rect;
+    for (let i = 0; i < arr.length; i++)
+    {
+        rect = arr[i].getBoundingClientRect();
+        arrCopy[i] = {height: rect.height.toString(), id: arr[i].id};
+    }
+    // Traverse the copyArr, i: 1st half element, j: second one
+    // If first half is out of scope
+    // Else if second half is out of scope
+    // Else if copyArr[i] > copyArr[j], assign copyArr[j] to arr
+    // Else assign copyArr[i] to arr
+    let i = left, j = mid+1;
+    for (let k = left; k <= right; k++)
+    {
+        if (i > mid) 
+        {
+            // arr[k] = arrCopy[j];
+            modifyBlock(arr[k], arrCopy[j].height, arrCopy[j].id)
+            j++;
+        }
+        else if (j > right) 
+        {
+            // arr[k] = arrCopy[i];
+            modifyBlock(arr[k], arrCopy[i].height, arrCopy[i].id)
+            i++;
+        }
+        else if (parseInt(arrCopy[i].id) > parseInt(arrCopy[j].id))
+        {
+            // arr[k] = arrCopy[j];
+            modifyBlock(arr[k], arrCopy[j].height, arrCopy[j].id)
+            j++;
+        }
+        else
+        {
+            // arr[k] = arrCopy[i];
+            modifyBlock(arr[k], arrCopy[i].height, arrCopy[i].id)
+            i++;
+        }
+    }
+}
+
+function modifyBlock(block, updatedHeight, updatedId) {
+    block.setAttribute("height", updatedHeight);
+    block.setAttribute("id", updatedId);
 }
 
 /* Trigger the play button */
 const button = document.getElementById("play");
 button.addEventListener("click", function() {
     let arr = document.getElementsByClassName("block");
-
-    let speed = getDelay();
-
+    DELAY = getDelay();
     // Disable control form
-    // disableControl();
-    mergeSort(speed, arr);
+    disableControl();
+    mergeSort(arr, 0, arr.length - 1);
 })
